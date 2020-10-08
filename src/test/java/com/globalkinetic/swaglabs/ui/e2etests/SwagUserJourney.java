ebd796a3e0e232d6b5fcf1f919de2c82f22a3c5c
raosaqib <rao.saqib@hotmail.com>
2020-10-08 09:07:15 +0300

    Update SwagUserJourney.java

--- src/test/java/com/globalkinetic/swaglabs/ui/e2etests/SwagUserJourney.java
+++ src/test/java/com/globalkinetic/swaglabs/ui/e2etests/SwagUserJourney.java
@@ -29,7 +29,6 @@ public class SwagUserJourney extends TestBase {
 	@Test(description = "validate user is able to login and able to add item to cart and able to click on checkout", priority = 10)
 	public void validateuserIsAbleToLoginToAppAndAbleToCheckOut(
 			@Optional("standard_user") String userName, @Optional("secret_sauce") String password ) throws Throwable {
-
 		swagLoginPage = new LoginPage(driver);
 		actualResult = swagLoginPage.getApplicationTitle();
 		soft.assertEquals(actualResult, ObjectReader.reader.getAppTitle());
@@ -39,14 +38,14 @@ public class SwagUserJourney extends TestBase {
 		super.logExtentReport("User is navigated to home page:: " + actualResult);
 		soft.assertEquals(actualResult, ObjectReader.reader.homepageheader(), "user is navigated to right application");
 
-		// ****************************************************************//
+		// ***************************New Page test starts*******************************//
 		status = homePgeObj.userIsAbleToAddItemToCart();
 		soft.assertEquals(status, true);
 		super.logExtentReport("user is able to add product to cart:: " + status);
 		status = homePgeObj.userIsAbleToClickOnCartIcon();
 		soft.assertEquals(status, true, "User is able to click on the cart icon");
 		super.logExtentReport("User is able to click on the cart icon:: " + status);
-		// ****************************************************************//
+		// *****************************New Page test starts*******************************//
 		cartpage = homePgeObj.UserNavigatesTocartpage();
 		super.logExtentReport("user is navigated to cart page:: " + cartpage);
 		status = cartpage.clickOnCheckOut();
